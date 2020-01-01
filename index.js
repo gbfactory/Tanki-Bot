85b993c264ca6892be3d2f089f2e810e10524203
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-01-01 23:14:41 +0100

    Random XP & new json file

--- index.js
+++ index.js
@@ -3,42 +3,42 @@ const client = new Discord.Client();
 const fs = require("fs");
 const prefix = '>';
 
-let xp = require("./storage/dgGeneral.json");
+let db = require("./storage/users.json");
 let lv = require("./storage/levels.json");
 
-client.on('message', message => {
+client.on('message', async message => {
 
-    // RANKS //
+    // xp x msg
+    var xpMsg = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    //console.log(xpMsg);
 
-    // Xp aggiungi ogni messaggio scritto.
-    let xpMsg = 1;
+    // LIVELLI v2019//
 
     // Se un utente è registrato nel database aggiunge xp e lvl.
-    if (xp[message.author.id]) {
-        xp[message.author.id].xp = xp[message.author.id].xp + xpMsg;
+    if (db[message.author.id]) {
+        db[message.author.id].xp += xpMsg;  //aumenta xp ogni msg
 
         // rankup
-        if (lv[xp[message.author.id].level + 1].exp <= xp[message.author.id].xp) {
-            xp[message.author.id].level = xp[message.author.id].level + 1;  // aumenta lvl
+        if (lv[db[message.author.id].level + 1].exp <= db[message.author.id].xp) {
+            db[message.author.id].level ++;  // aumenta lvl
+            db[message.author.id].cry +=lv[db[message.author.id].level].crystals    // aumenta crys
 
             let lvupEmbed = new Discord.RichEmbed()
                 .setColor("#ffc300")
-                .setThumbnail(lv[xp[message.author.id].level].image)
-                .addField("✨ Rank up! ✨", `Congratulazioni **${message.author.username}**! \nOra sei al rank **${lv[xp[message.author.id].level].name}** \n+${lv[xp[message.author.id].level].crystals} 💎`)
-
-            xp[message.author.id].cry = xp[message.author.id].cry + lv[xp[message.author.id].level].crystals    // aumenta crys
+                .setThumbnail(lv[db[message.author.id].level].image)
+                .addField("✨ Rank up! ✨", `Congratulazioni **${message.author.username}**! \nOra sei al rank **${lv[db[message.author.id].level].name}** \n+${lv[db[message.author.id].level].crystals} 💎`)
 
             message.channel.send({embed:lvupEmbed});
         }
 
-        fs.writeFile("./storage/dbGeneral.json", JSON.stringify(xp), (err) => {
+        fs.writeFile("./storage/users.json", JSON.stringify(db), (err) => {
             if (err) console.log(err)
         });
 
     }
 
 
-    // COMMAND HANDLER //
+    // COMMAND HANDLER v2018//
 
     let msg = message.content.toUpperCase();
     let sender = message.author;
@@ -48,25 +48,38 @@ client.on('message', message => {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
 
+    let youCant = new Discord.RichEmbed()
+    .setAuthor("Non puoi usare il bot in questo canale!")
+    .setColor("#87d704");
+
+    // blocco msg nel canale general di italian culture server
+    if (message.channel.id === '488001350320259076'){
+        message.channel.send({embed:youCant});
+        return;
+    }
+    // end blocco msg
+
     try {
         let commandFile = require(`./commands/${cmd}.js`);
         commandFile.run(client, message, args);
     } catch (e) {
         console.log(e.message);
     } finally {
-        console.log(`${message.author.tag} ha usato il comando ${cmd}`);
+        console.log(`${message.author.tag} used ${cmd}`);
     }
 
 });
+
 
 // AVVIO BOT //
 
 client.on('ready', async () => {
     console.log('Bot avviato!');
-    client.user.setActivity("TankiBot", {
-        type: "WATCHING"
+    client.user.setActivity("Tanki Online", {
+        type: "PLAYING"
     });
 });
+
 
 // TOKEN //
 
