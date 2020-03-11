4d473a28ee97728bd0b014bf7130bda151b71d38
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-03-11 12:46:46 +0100

    Added "special" section

--- commands/buy.js
+++ commands/buy.js
@@ -1,56 +1,97 @@
+/**
+ * Tanki Bot. ECONOMY.
+ * 
+ * Command from buying items from the bot own shop.
+ * 
+ * @author gbfactory
+ * @since  06.01.2020
+*/
+
 const Discord = require("discord.js");
-const fs = require("fs");
 
-let db = require("../storage/users.json");
+module.exports.run = async (client, message, args, con) => {
 
-module.exports.run = async (client, message, args) => {
+    let authorId = message.author.id;
 
-    if (!db[message.author.id]) {
-        let rgNo  = new Discord.RichEmbed()
-            .setAuthor("You aren't registered! Use >register (username) to create a profile.")
-            .setColor("#f54242");
-        message.channel.send({embed:rgNo});
-        return;
-    }
+    con.query(`SELECT * FROM users WHERE id = '${authorId}'`, (err, rows) => {
+        if (err) throw err;
 
-    if (!args[0]) {
-        let shop = new Discord.RichEmbed()
-            .setColor("#f54242")
-            .setAuthor("Wrong command usage! Use >help for more informations.")
-        message.channel.send({embed:shop});
-        return;
-    }
+        var rowsUsers = rows;
 
-    if (!isNaN(args[0]) && args[0] > 0) {
-
-        prezzo = args[0] * 1000;
-
-        if(db[message.author.id].crys >= prezzo) {
-            let bought = new Discord.RichEmbed()
-                .setColor("#87d704")
-                .setThumbnail('https://i.imgur.com/CtoiatU.png')
-                .setAuthor(`You bought ${args[0]} containers for ${prezzo} 💎`);
-
-            db[message.author.id].crys -= prezzo;
-            db[message.author.id].containers.container += args[0];
-
-            fs.writeFile("./storage/users.json", JSON.stringify(db), (err) => {
-                if(err) console.log(err)
-            });
-
-            message.channel.send({embed:bought});
-        } else {
-            let noMoney = new Discord.RichEmbed()
-                .setColor("#f54242")
-                .setAuthor("You don't have crystals!");
-            message.channel.send({embed:noMoney});
+        if (rows.length < 1) {
+            let rgNo  = new Discord.RichEmbed()
+                .setAuthor("You aren't registered! Use >register (username) to create a profile.")
+                .setColor("#f54242");
+            message.channel.send({embed:rgNo});
+            return;
         }
 
-    } else {
-        let nan = new Discord.RichEmbed()
+        if (!args[0]) {
+            let shop = new Discord.RichEmbed()
                 .setColor("#f54242")
-                .setAuthor("Not a valid input!");
-        message.channel.send({embed:nan});
-    } 
+                .setAuthor("Wrong command usage! Use >help for more informations.")
+            message.channel.send({embed:shop});
+            return;
+        }
+
+        if (!isNaN(args[0]) && args[0] > 0) {
+
+            prezzo = args[0] * 5000;
+
+            if(rowsUsers[0].crys >= prezzo) {
+
+                let bought = new Discord.RichEmbed()
+                    .setColor("#87d704")
+                    .setThumbnail('https://i.imgur.com/CtoiatU.png')
+                    .setAuthor(`You bought ${args[0]} containers for ${prezzo} 💎`);
+
+                con.query(`UPDATE users SET crys = ${rowsUsers[0].crys - prezzo} WHERE id = '${authorId}'`);
+
+                con.query(`SELECT * FROM items WHERE id = '${authorId}'`, (err, rows) => {
+                    if (err) throw err;
+                    con.query(`UPDATE items SET containers = ${rows[0].containers + args[0]} WHERE id = '${authorId}'`);
+                    
+                    message.channel.send({embed:bought});
+                });
+
+            } else {
+                let noMoney = new Discord.RichEmbed()
+                    .setColor("#f54242")
+                    .setAuthor("You don't have enough Crystals!");
+                message.channel.send({embed:noMoney});
+            }
+        } else if (args[0].toUpperCase() === "RENAME" && args[1].toUpperCase() === "PASS") {
+
+            if (rowsUsers[0].tankoins >= 1000) {
+
+                let bought = new Discord.RichEmbed()
+                    .setColor("#87d704")
+                    .setThumbnail('https://en.tankiwiki.com/images/en/9/9c/Rename_pass_preview.png')
+                    .setAuthor(`You bought a Rename Pass for 2000 Tankoins`);
+
+                con.query(`UPDATE users SET tankoins = ${rowsUsers[0].tankoins - 2000} WHERE id = '${authorId}'`);
+
+                con.query(`SELECT coinbox FROM items WHERE id = '${authorId}'`, (err, rows) => {
+                    if (err) throw err;
+                    con.query(`UPDATE items SET coinbox = ${rows[0].coinbox + 1} WHERE id = '${authorId}'`);
+                    
+                    message.channel.send({embed:bought});
+                });
+                
+            } else {
+                let noMoney = new Discord.RichEmbed()
+                    .setColor("#f54242")
+                    .setAuthor("You don't have enough Tankoins!");
+                message.channel.send({embed:noMoney});
+            }
+
+        } else {
+            let nan = new Discord.RichEmbed()
+                .setColor("#f54242")
+                .setAuthor("Use >shop to see what you can buy!");
+            message.channel.send({embed:nan});
+        } 
+
+    });
 
 }
