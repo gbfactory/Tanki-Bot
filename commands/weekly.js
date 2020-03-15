6115c7233af96af0f086c5f62bb76ffc1203721c
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-03-15 01:36:26 +0100

    Fixed arrow displaying with no value

--- commands/weekly.js
+++ commands/weekly.js
@@ -1,90 +1,112 @@
+/**
+ * Tanki Bot.
+ * 
+ * RATINGS: get players' weekly statistics
+ * 
+ * @author gbfactory
+ * @since  09.08.2017
+*/
+
 const Discord = require("discord.js");
 const snekfetch = require("snekfetch");
 
 const api = "https://ratings.tankionline.com/get_stat/profile/?user=";
 
-module.exports.run = async (client, message, args, tools) => {
+module.exports.run = async(client, message, args, tools) => {
 
-    nome = args[0];
+    var nickname = args[0];
 
-    if (!nome) {
-      let nonUsNome = new Discord.RichEmbed()
-        .setAuthor("You didn't put a nickname.")
-        .setColor("#87d704");
-        message.channel.send({embed:nonUsName});
-        return;
+    if (!nickname) {
+        let noNickname = new Discord.RichEmbed()
+        .setAuthor('You have to specify a nickname >ratings (nickname)')
+        .setColor('#f54242');
 
-    } else {
-        snekfetch.get(api + args[0]).then(r =>{
-
-            if (!r.body.response) {
-              let nonEsiste = new Discord.RichEmbed()
-                .setAuthor("This user doesn't exist.")
-                .setColor("#87d704");
-              message.channel.send({embed:nonEsiste});
-              return;
-
-            } else {
-
-                var nome = r.body.response.name;
-
-                //ratings correnti
-                //posizione
-                var curPosCry = ((r.body.response.rating.crystals.position).toLocaleString('en')).replace("-1", "—");
-                var curPosGold = ((r.body.response.rating.golds.position).toLocaleString('en')).replace("-1", "—");
-                var curPosExp = ((r.body.response.rating.score.position).toLocaleString('en')).replace("-1", "—");
-                var curPosEff = ((r.body.response.rating.efficiency.position).toLocaleString('en')).replace("-1", "—");
-
-                //valore
-                var curValCry = ((r.body.response.rating.crystals.value).toLocaleString('en')).replace("-1", "—");
-                var curValGold = ((r.body.response.rating.golds.value).toLocaleString('en')).replace("-1", "—");
-                var curValExp = ((r.body.response.rating.score.value).toLocaleString('en')).replace("-1", "—");
-                var curValEff = ((r.body.response.rating.efficiency.value).toLocaleString('en')).replace("-1", "—");
-
-                //rating precedenti
-                //posizione
-                var precPosCry = ((r.body.response.previousRating.crystals.position).toLocaleString('en')).replace("-1", "—");
-                var precPosGold = ((r.body.response.previousRating.golds.position).toLocaleString('en')).replace("-1", "—");
-                var precPosExp = ((r.body.response.previousRating.score.position).toLocaleString('en')).replace("-1", "—");
-
-                //valore
-                var precValCry = ((r.body.response.previousRating.crystals.value).toLocaleString('en')).replace("-1", "—");
-                var precValGold = ((r.body.response.previousRating.golds.value).toLocaleString('en')).replace("-1", "—");
-                var precValExp = ((r.body.response.previousRating.score.value).toLocaleString('en')).replace("-1", "—");
-
-                //confronto ratings
-                if (curValCry > precValCry) {
-                  diffCry = "▲";
-                } else {
-                  diffCry = "▼";
-                }
-
-                if (curValGold > precValGold) {
-                  diffGold = "▲";
-                } else {
-                  diffGold = "▼";
-                }
-
-                if (curValExp > precValExp) {
-                  diffExp = "▲";
-                } else {
-                  diffExp = "▼";
-                }
-
-                //embed
-                let embed = new Discord.RichEmbed()
-                .setTitle("Tanki Online Ratings - Weekly Positions of " + nome)
-                .setURL("https://ratings.tankionline.com/en/user/" + nome)
-                .setFooter("Bot by gb_factory")
-        				.setThumbnail("https://i.imgur.com/ifOqPcp.png")
-                .setColor("#00c2ff")
-                .setTimestamp()
-                .addField("Experience", `**Place**: ${curPosExp} \n**Value**: ${curValExp} ${diffExp} \n**Previusly**: ${precValExp}`)
-                .addField("Gold Boxes", `**Place**: ${curPosGold} \n**Value**: ${curValGold} ${diffGold} \n**Previusly**: ${precValGold}`)
-                .addField("Crystals", `**Place**: ${curPosCry} \n**Value**: ${curValCry} ${diffCry} \n**Previusly**: ${precValCry}`)
-                .addField("Efficiency", `**Place**: ${curPosEff} \n**Value**: ${curValEff} \n**Previusly**: —`)
-                message.channel.send({embed:embed})
-            }
-        })
+        return message.channel.send({embed:noNickname});
     }
+
+    snekfetch.get(api + nickname).then(r => {
+
+        if (r.body.responseType === 'NOT_FOUND') {
+            let noUser = new Discord.RichEmbed()
+            .setAuthor('Player not found!')
+            .setColor('#f54242');
+
+            return message.channel.send({embed:noUser});
+        } else if (!r.body.responseType) {
+            let noApi = new Discord.RichEmbed()
+            .setAuthor('Tanki Online API unavailable. Try again later.')
+            .setColor('#f54242');
+            return message.channel.send({embed:noApi});
+        }
+
+        var res = r.body.response;
+
+        // Name
+        var name = res.name;
+
+        // Current Ratings
+        // Position
+        var curPosCry = ((res.rating.crystals.position).toLocaleString('en')).replace("-1", "—");
+        var curPosGold = ((res.rating.golds.position).toLocaleString('en')).replace("-1", "—");
+        var curPosExp = ((res.rating.score.position).toLocaleString('en')).replace("-1", "—");
+        var curPosEff = ((res.rating.efficiency.position).toLocaleString('en')).replace("-1", "—");
+
+        // Value
+        var curValCry = ((res.rating.crystals.value).toLocaleString('en')).replace("-1", "—");
+        var curValGold = ((res.rating.golds.value).toLocaleString('en')).replace("-1", "—");
+        var curValExp = ((res.rating.score.value).toLocaleString('en')).replace("-1", "—");
+        var curValEff = ((res.rating.efficiency.value).toLocaleString('en')).replace("-1", "—");
+
+        // Old Ratings
+        // Position
+        var precPosCry = ((res.previousRating.crystals.position).toLocaleString('en')).replace("-1", "—");
+        var precPosGold = ((res.previousRating.golds.position).toLocaleString('en')).replace("-1", "—");
+        var precPosExp = ((res.previousRating.score.position).toLocaleString('en')).replace("-1", "—");
+
+        // Value
+        var precValCry = ((res.previousRating.crystals.value).toLocaleString('en')).replace("-1", "—");
+        var precValGold = ((res.previousRating.golds.value).toLocaleString('en')).replace("-1", "—");
+        var precValExp = ((res.previousRating.score.value).toLocaleString('en')).replace("-1", "—");
+
+        // Compare
+
+        var diffCry = "";
+        var diffGold = "";
+        var diffExp = "";
+        
+        if (curValCry > precValCry) {
+            diffCry = "▲";
+        } else if (curValCry < precValCry && curValCry > 0) {
+            diffCry = "▼";
+        }
+
+        if (curValGold > precValGold) {
+            diffGold = "▲";
+        } else if (curValGold < precValGold && curValGold > 0) {
+            diffGold = "▼";
+        }
+
+        if (curValExp > precValExp) {
+            diffExp = "▲";
+        } else if (curValExp < precValExp && curValExp > 0) {
+            diffExp = "▼";
+        }
+
+        // Embed
+        let embed = new Discord.RichEmbed()
+            .setAuthor('Tanki Bot')
+            .setTitle(`Ratings - Weekly Ratings`)
+            .setDescription(`Profile of ${name}`)
+            .setThumbnail("https://i.imgur.com/ifOqPcp.png")
+            .setColor("#00c2ff")
+            .setTimestamp()
+            .addField("Experience", `**Place**: ${curPosExp} \n**Value**: ${curValExp} ${diffExp} \n**Previusly**: ${precValExp}`, true)
+            .addField("Gold Boxes", `**Place**: ${curPosGold} \n**Value**: ${curValGold} ${diffGold} \n**Previusly**: ${precValGold}`, true)
+            .addField("Crystals", `**Place**: ${curPosCry} \n**Value**: ${curValCry} ${diffCry} \n**Previusly**: ${precValCry}`, true)
+            .addField("Efficiency", `**Place**: ${curPosEff} \n**Value**: ${curValEff} \n**Previusly**: —`, true)
+        
+        message.channel.send({ embed: embed })
+    
+    })
+
 }
