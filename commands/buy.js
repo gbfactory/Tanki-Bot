e38c06ec2a100c3f00da0d0472c03261b5778bc5
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-04-04 23:01:20 +0200

    Added emojis and removed useless code

--- commands/buy.js
+++ commands/buy.js
@@ -8,6 +8,8 @@
 */
 
 const Discord = require("discord.js");
+let color = require('../storage/colors.json');
+let emoji = require('../storage/emoji.json');
 
 module.exports.run = async (client, message, args, con) => {
 
@@ -20,17 +22,10 @@ module.exports.run = async (client, message, args, con) => {
 
         if (rows.length < 1) {
             let rgNo  = new Discord.RichEmbed()
-                .setAuthor("You aren't registered! Use >register (username) to create a profile.")
-                .setColor("#f54242");
+                .setAuthor("Tanki Bot")
+                .setDescription("❗️ You aren't registered! \nUse `>register (username)` to create a profile")
+                .setColor(color.red);
             message.channel.send({embed:rgNo});
-            return;
-        }
-
-        if (!args[0]) {
-            let shop = new Discord.RichEmbed()
-                .setColor("#f54242")
-                .setAuthor("Wrong command usage! Use >help for more informations.")
-            message.channel.send({embed:shop});
             return;
         }
 
@@ -42,9 +37,9 @@ module.exports.run = async (client, message, args, con) => {
             if(rowsUsers[0].crys >= prezzo) {
 
                 let bought = new Discord.RichEmbed()
-                    .setColor("#87d704")
                     .setThumbnail('https://i.imgur.com/CtoiatU.png')
-                    .setAuthor(`You bought ${quantity} containers for ${prezzo} 💎`);
+                    .setAuthor(`✔️ You bought ${quantity} containers for ${prezzo} 💎`)
+                    .setColor(color.green);
 
                 con.query(`UPDATE users SET crys = ${parseInt(rowsUsers[0].crys) - prezzo} WHERE id = '${authorId}'`);
 
@@ -58,8 +53,8 @@ module.exports.run = async (client, message, args, con) => {
 
             } else {
                 let noMoney = new Discord.RichEmbed()
-                    .setColor("#f54242")
-                    .setAuthor("You don't have enough Crystals!");
+                    .setAuthor("❌ You don't have enough Crystals 💎!")
+                    .setColor(color.red)
                 message.channel.send({embed:noMoney});
             }
         } else if (args[0].toUpperCase() === "RENAME" && args[1].toUpperCase() === "PASS") {
@@ -67,9 +62,9 @@ module.exports.run = async (client, message, args, con) => {
             if (rowsUsers[0].tankoins >= 1000) {
 
                 let bought = new Discord.RichEmbed()
-                    .setColor("#87d704")
                     .setThumbnail('https://en.tankiwiki.com/images/en/9/9c/Rename_pass_preview.png')
-                    .setAuthor(`You bought a Rename Pass for 2000 Tankoins`);
+                    .setAuthor(`✔️ You bought a Rename Pass for 2000 Tankoins ${emoji.tankoins}`)
+                    .setColor(color.green)
 
                 con.query(`UPDATE users SET tankoins = ${parseInt(rowsUsers[0].tankoins) - 2000} WHERE id = '${authorId}'`);
 
@@ -82,16 +77,19 @@ module.exports.run = async (client, message, args, con) => {
                 
             } else {
                 let noMoney = new Discord.RichEmbed()
-                    .setColor("#f54242")
-                    .setAuthor("You don't have enough Tankoins!");
+                    .setAuthor(`❌ You don't have enough Tankoins ${emoji.tankoins} 💎!`)
+                    .setColor(color.red)
                 message.channel.send({embed:noMoney});
             }
 
         } else {
-            let nan = new Discord.RichEmbed()
-                .setColor("#f54242")
-                .setAuthor("Use >shop to see what you can buy!");
-            message.channel.send({embed:nan});
+            let gethelp = new Discord.RichEmbed()
+                .setAuthor("Tanki Bot")
+                .setTitle("💰 Shop")
+                .setThumbnail("https://i.imgur.com/ZOclxPD.png")
+                .addField("📁 Usage", "📌 `>shop`: list of available items \n📌 `>buy (item)`: buy a specific item")
+                .setColor(color.darkBlue)
+            message.channel.send({embed:gethelp});
         } 
 
     });
