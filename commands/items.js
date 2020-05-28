9418895d870f08fea7408b617971a6e3163e9c0f
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-05-28 20:10:52 +0200

    Completed command

--- commands/items.js
+++ commands/items.js
@@ -7,8 +7,10 @@
  * @since 04.05.2020
  */
 
-import Discord from 'discord.js';
-import snektetch from 'snekfetch';
+const Discord = require('discord.js');
+const snekfetch = require('snekfetch');
+
+const api = 'https://ratings.tankionline.com/api/eu/profile/?user=';
 
 module.exports.run = async(client, message, args, tools) => {
 
@@ -50,6 +52,20 @@ module.exports.run = async(client, message, args, tools) => {
         var modules = res.resistanceModules;
         var turrets = res.turretsPlayed;
 
+        // Function to remove from array duplicate items with the same name but different level. 
+        // Tanki stores all the previous modifications of the items, not only the current one.
+        function fixDupes(arr) {
+            var newArr = [];
+            for (let i = 0; i < arr.length; i++) {
+                if (i == 0) {
+                    newArr.push(arr[i]);
+                } else if (arr[i]['name'] != arr[i - 1]['name']) {
+                    newArr.push(arr[i]);
+                }
+            }
+            return newArr;
+        }
+        
         // EMBED
         let itemsEmbed = new Discord.RichEmbed()
             .setAuthor('Tanki Bot')
@@ -57,12 +73,12 @@ module.exports.run = async(client, message, args, tools) => {
             .setDescription(`Items of ${name}`)
             .setColor('#29ad1d')
             .setTimestamp()
-            .addField('<:Icon_battery_supply:605414662506414110> Drones', drones.length, true)
-            .addField('<:burstDamageIcon:661186313176875031> Turrets', turrets.length, true)
-            .addField('<:Hornet:605414772003045395> Hulls', hulls.length, true)
+            .addField('<:burstDamageIcon:661186313176875031> Turrets', fixDupes(turrets).length, true)
+            .addField('<:Hornet:605414772003045395> Hulls', fixDupes(hulls).length, true)
             .addField('<:Complect_paint_tro:706755019365679124> Paints', paints.length, true)
-            .addField('<:Spectrum_prot_symbol:706753913461932052> Modules', modules.length, true)
-            .addField('<:Teddy_Bear:706754544821862400> Gifts Recived', presents.length, true)        
+            .addField('<:Icon_battery_supply:605414662506414110> Drones', fixDupes(drones).length, true)
+            .addField('<:Spectrum_prot_symbol:706753913461932052> Modules', fixDupes(modules).length, true)
+            .addField('<:Teddy_Bear:706754544821862400> Gifts Recived', presents.length, true)
 
         message.channel.send({embed:itemsEmbed});
 
