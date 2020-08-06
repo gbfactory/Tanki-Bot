31d3f1481ea21398115a54774915b108ed6c71dc
gb-factory <gb.factory@hotmail.com>
2020-08-06 12:39:51 +0200

    ⚡️ Improve command - switch and vars

--- commands/economy/sell.js
+++ commands/economy/sell.js
@@ -17,7 +17,7 @@ module.exports = {
     cooldown: 3,
     execute(client, message, args, con) {
 
-        var authorId = message.author.id;
+        const authorId = message.author.id;
 
         function random(min, max) {
             return Math.floor(Math.random() * (max - min + 1) + min);
@@ -41,7 +41,7 @@ module.exports = {
         con.query(`SELECT id, crys FROM users WHERE id = ${authorId}`, (err, rows) => {
             if (err) throw err;
 
-            var rowsUsers = rows;
+            const rowsUsers = rows;
 
             if (rows.length < 1) {
                 let rgNo = new Discord.RichEmbed()
@@ -54,79 +54,91 @@ module.exports = {
             con.query(`SELECT * FROM items WHERE id = ${authorId}`, (err, rows) => {
                 if (err) throw err;
 
-                if (args[0] === 'all') {
+                let price, newPrice;
 
-                    var repair = rows[0].repair;
-                    var armor = rows[0].armor;
-                    var damage = rows[0].damage;
-                    var nitro = rows[0].nitro;
-                    var mine = rows[0].mine;
-                    var battery = rows[0].battery;
-                    var rare = rows[0].rare;
-                    var epic = rows[0].epic;
-                    var legendary = rows[0].legendary;
-                    var turrets = rows[0].skinTurrets;
-                    var hulls = rows[0].skinHulls;
+                let repair, armor, damage, nitro, mine, battery, rare, epic, legendary, turrets, hulls;
 
-                    var price = (repair * 50) + (armor * 25) + (damage * 25) + (nitro * 25) + (mine * 25) + (battery * 60) + (rare * random(2000, 4000)) + (epic * random(4000, 8000)) + (legendary * random(8000, 16000)) + (turrets * 100000) + (hulls * 100000);
+                switch (args[0]) {
+                    case 'all':
+                        repair = rows[0].repair;
+                        armor = rows[0].armor;
+                        damage = rows[0].damage;
+                        nitro = rows[0].nitro;
+                        mine = rows[0].mine;
+                        battery = rows[0].battery;
+                        rare = rows[0].rare;
+                        epic = rows[0].epic;
+                        legendary = rows[0].legendary;
+                        turrets = rows[0].skinTurrets;
+                        hulls = rows[0].skinHulls;
 
-                    var newPrice = rowsUsers[0].crys + price;
+                        price = (repair * 50) + (armor * 25) + (damage * 25) + (nitro * 25) + (mine * 25) + (battery * 60) + (rare * random(2000, 4000)) + (epic * random(4000, 8000)) + (legendary * random(8000, 16000)) + (turrets * 100000) + (hulls * 100000);
 
-                    con.query(`UPDATE users SET crys = ${newPrice} WHERE id = ${authorId}`);
+                        newPrice = rowsUsers[0].crys + price;
 
-                    con.query(`UPDATE items SET repair = 0, armor = 0, damage = 0, nitro = 0, mine = 0, battery = 0, rare = 0, epic = 0, legendary = 0, skinTurrets = 0, skinHulls = 0 WHERE id = ${authorId}`);
+                        con.query(`UPDATE users SET crys = ${newPrice} WHERE id = ${authorId}`);
 
-                    message.channel.send({ embed: embedGenerator('all', price) });
+                        con.query(`UPDATE items SET repair = 0, armor = 0, damage = 0, nitro = 0, mine = 0, battery = 0, rare = 0, epic = 0, legendary = 0, skinTurrets = 0, skinHulls = 0 WHERE id = ${authorId}`);
 
-                } else if (args[0] === 'supplies') {
+                        message.channel.send({ embed: embedGenerator('all', price) });
+                        
+                        break;
 
-                    var repair = rows[0].repair;
-                    var armor = rows[0].armor;
-                    var damage = rows[0].damage;
-                    var nitro = rows[0].nitro;
-                    var mine = rows[0].mine;
-                    var battery = rows[0].battery;
+                    case 'supplies':
+                        repair = rows[0].repair;
+                        armor = rows[0].armor;
+                        damage = rows[0].damage;
+                        nitro = rows[0].nitro;
+                        mine = rows[0].mine;
+                        battery = rows[0].battery;
+    
+                        price = (repair * 50) + (armor * 25) + (damage * 25) + (nitro * 25) + (mine * 25) + (battery * 60);
+    
+                        newPrice = rowsUsers[0].crys + price;
+    
+                        con.query(`UPDATE users SET crys = ${newPrice} WHERE id = ${authorId}`);
+    
+                        con.query(`UPDATE items SET repair = 0, armor = 0, damage = 0, nitro = 0, mine = 0, battery = 0 WHERE id = ${authorId}`);
+    
+                        message.channel.send({ embed: embedGenerator('supplies', price) });
 
-                    var price = (repair * 50) + (armor * 25) + (damage * 25) + (nitro * 25) + (mine * 25) + (battery * 60);
+                        break;
+                    
+                    case 'paints':
+                        rare = rows[0].rare;
+                        epic = rows[0].epic;
+                        legendary = rows[0].legendary;
 
-                    var newPrice = rowsUsers[0].crys + price;
+                        price = (rare * random(2000, 4000)) + (epic * random(4000, 8000)) + (legendary * random(8000, 16000));
 
-                    con.query(`UPDATE users SET crys = ${newPrice} WHERE id = ${authorId}`);
+                        newPrice = rowsUsers[0].crys + price;
 
-                    con.query(`UPDATE items SET repair = 0, armor = 0, damage = 0, nitro = 0, mine = 0, battery = 0 WHERE id = ${authorId}`);
+                        con.query(`UPDATE users SET crys = ${newPrice} WHERE id = ${authorId}`);
 
-                    message.channel.send({ embed: embedGenerator('supplies', price) });
+                        con.query(`UPDATE items SET rare = 0, epic = 0, legendary = 0 WHERE id = ${authorId}`);
 
-                } else if (args[0] === 'paints') {
+                        message.channel.send({ embed: embedGenerator('paints', price) });
 
-                    var rare = rows[0].rare;
-                    var epic = rows[0].epic;
-                    var legendary = rows[0].legendary;
+                        break;
 
-                    var price = (rare * random(2000, 4000)) + (epic * random(4000, 8000)) + (legendary * random(8000, 16000));
+                    case 'skins':
+                        turrets = rows[0].skinTurrets;
+                        hulls = rows[0].skinHulls;
+    
+                        price = (turrets * 100000) + (hulls * 100000);
+    
+                        newPrice = rowsUsers[0].crys + price;
+    
+                        con.query(`UPDATE users SET crys = ${newPrice} WHERE id = ${authorId}`);
+    
+                        con.query(`UPDATE items SET skinTurrets = 0, skinHulls = 0 WHERE id = ${authorId}`);
+    
+                        message.channel.send({ embed: embedGenerator('skins', price) });
 
-                    var newPrice = rowsUsers[0].crys + price;
+                        break;
 
-                    con.query(`UPDATE users SET crys = ${newPrice} WHERE id = ${authorId}`);
-
-                    con.query(`UPDATE items SET rare = 0, epic = 0, legendary = 0 WHERE id = ${authorId}`);
-
-                    message.channel.send({ embed: embedGenerator('paints', price) });
-
-                } else if (args[0] === 'skins') {
-
-                    var turrets = rows[0].skinTurrets;
-                    var hulls = rows[0].skinHulls;
-
-                    var price = (turrets * 100000) + (hulls * 100000);
-
-                    var newPrice = rowsUsers[0].crys + price;
-
-                    con.query(`UPDATE users SET crys = ${newPrice} WHERE id = ${authorId}`);
-
-                    con.query(`UPDATE items SET skinTurrets = 0, skinHulls = 0 WHERE id = ${authorId}`);
-
-                    message.channel.send({ embed: embedGenerator('skins', price) });
+                    default:
+                        break;
                 }
 
             });
