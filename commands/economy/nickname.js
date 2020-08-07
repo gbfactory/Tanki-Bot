3870dbe4b9746dc0c6feeee7f29460b9b7e255cb
gb-factory <gb.factory@hotmail.com>
2020-08-07 19:10:41 +0200

    ✨ Updated command

--- commands/economy/nickname.js
+++ commands/economy/nickname.js
@@ -12,12 +12,12 @@ const Discord = require("discord.js");
 module.exports = {
     name: 'nickname',
     description: 'Set your real Tanki nickname so people can see who you are in the game.',
-    usage: '`>nickname [nickname]`',
-    args: true,
+    usage: '`>nickname` Check your nickname \n`>nickname [nickname]` Set your nickname',
     cooldown: 3,
     execute(client, message, args, con) {
 
         let authorId = message.author.id;
+        let userNick = args[0];
 
         con.query(`SELECT * FROM users WHERE id = '${authorId}'`, (err, rows) => {
             if (err) throw err;
@@ -30,38 +30,41 @@ module.exports = {
                 return;
             }
 
-            if (args[0].length > 20) {
-                let long = new Discord.RichEmbed()
-                    .setAuthor("Your tanki nickname can't be longer than 20 characters")
-                    .setColor("#f54242")
-                message.channel.send({ embed: long });
-                return;
+            // Check your actual nickname
+            if (!userNick) {
+
+                let oldNick = rows[0].nick ? rows[0].nick : 'Not set';
+                
+                let checkEmbed = new Discord.RichEmbed()
+                    .setAuthor('Tanki Bot')
+                    .setTitle('Nickname')
+                    .setDescription('Set your Tanki Nickname to check your ratings faster and let other people know who you are.')
+                    .addField('Your Nickname', oldNick)
+                    .addField('Update your Nickname', '`>nickname [new nickname]`')
+                    .setThumbnail('https://i.imgur.com/t9aW1ri.png')
+                
+                return message.channel.send({ embed:checkEmbed });
+
             }
 
-            if (args[0].length < 3) {
-                let short = new Discord.RichEmbed()
-                    .setAuthor("Your tanki nickname can't be shorter that 3 characters")
-                    .setColor("#f54242")
-                message.channel.send({ embed: short });
-                return;
-            }
-
-            if (args[0].match(/^([0-9]|[a-z])+([0-9a-z]+)$/i)) {
-
-                con.query(`UPDATE users SET nick = ? WHERE id = '${authorId}'`, [args[0]]);
-
-                let set = new Discord.RichEmbed()
-                    .setColor("#00ffff")
-                    .setAuthor("You set your nickname to " + args[0])
-                message.channel.send({ embed: set });
-                return;
-            } else {
+            // Check if the nickname is valid
+            if (!userNick.match(/^(?=[a-zA-Z0-9-_]{3,20}$)(?!.*[_-]{2})[^_-].*[^_-]$/i)) {
                 let regIllegal = new Discord.RichEmbed()
-                    .setAuthor("Your nickname must be alpanumerical!")
+                    .setAuthor('Invalid nickname!')
+                    .setDescription('Your nickname can contain only letters, numbers and symbols (_, -). It must be between 3 and 20 characters.')
                     .setColor("#f54242");
                 message.channel.send({ embed: regIllegal });
-                return;
+                return;   
             }
+
+            // Update the nickname
+            con.query(`UPDATE users SET nick = ? WHERE id = '${authorId}'`, userNick);
+
+            let set = new Discord.RichEmbed()
+                .setColor("#00ffff")
+                .setAuthor("You set your nickname to " + userNick)
+            message.channel.send({ embed: set });
+            return;
 
         })
 
