458c152023dbb3763307c9bb1fa0a9895fb84346
gb-factory <gb.factory@hotmail.com>
2020-08-31 18:20:33 +0200

    ✨ Add rename command

---
+++ commands/economy/rename.js
@@ -0,0 +1,55 @@
+/**
+ * Tanki Bot
+ * 
+ * Command to change the user bot registration username
+ * 
+ * @author gbfactory
+ * @since 06 08 2020
+ */
+
+const Discord = require("discord.js");
+
+module.exports = {
+    name: 'rename',
+    description: 'Change the username you used to register to the Bot. Requires a Rename Pass',
+    usage: '`>rename [new username]`',
+    args: true,
+    cooldown: 3,
+    execute(client, message, args, con) {
+
+        let authorId = message.author.id;
+
+        con.query(`SELECT * FROM users WHERE id = '${authorId}'`, (err, rows) => {
+            if (err) throw err;
+
+            if (rows.length < 1) {
+                let rgNo = new Discord.RichEmbed()
+                    .setAuthor("You aren't registered! Use >register (username) to create a profile.")
+                    .setColor("#f54242");
+                message.channel.send({ embed: rgNo });
+                return;
+            }
+
+            // Check if the nickname is valid
+            if (!args[0].match(/^(?=[a-zA-Z0-9-_]{3,20}$)(?!.*[_-]{2})[^_-].*[^_-]$/i)) {
+                let regIllegal = new Discord.RichEmbed()
+                    .setAuthor('Invalid nickname!')
+                    .setDescription('Your nickname can contain only letters, numbers and symbols (_, -). It must be between 3 and 20 characters.')
+                    .setColor("#f54242");
+                message.channel.send({ embed: regIllegal });
+                return;
+            }
+
+            // Update the nickname
+            con.query(`UPDATE users SET nick = ? WHERE id = '${authorId}'`, [args[0]]);
+
+            let set = new Discord.RichEmbed()
+                .setColor("#00ffff")
+                .setAuthor("You set your nickname to " + args[0])
+            message.channel.send({ embed: set });
+            return;
+
+        })
+        
+    },
+};
