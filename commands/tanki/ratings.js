4929127c40107b1b6ec14983425b751c6a5c350a
gb-factory <gb.factory@hotmail.com>
2020-09-07 13:03:37 +0200

    💄 Added thousands separators

--- commands/tanki/ratings.js
+++ commands/tanki/ratings.js
@@ -112,9 +112,9 @@ module.exports = {
                 .setTimestamp()
                 .addField(`Name`, `<:elmetto:660442439441448981> ${name}`, true)
                 .addField(`Rank`, `${premiumBanner} ${rankEmoji} ${rankName}`, true)
-                .addField(`<:xp:661186205458628608> Score`, `${scoreBase} 🔸 ${score} (-${scoreLeft}) 🔸 ${scoreNext}`)
-                .addField(`Info`, `💎 Crystals: ${earnedCrystals} \n<:gold:660257810797428776> Gold Boxes ${caughtGolds} \n🕐 Game Time: ${gameTime} \n<:sups:660260925546168404> Supplies: ${suppliesUsage}`, true)
-                .addField(`Stats`, `<:Clan_destroyed:660950530096496661> Kills: ${kills} \n<:Clan_death:660950530071461890> Deaths: ${deaths} \n<:Clan_kd:660950530293497866> K/D: ${kd} \n⚙️ GearScore: ${gearScore}`, true)
+                .addField(`<:xp:661186205458628608> Score`, `${scoreBase.toLocaleString()} 🔸 ${score.toLocaleString()} (-${scoreLeft.toLocaleString()}) 🔸 ${scoreNext.toLocaleString()}`)
+                .addField(`Info`, `💎 Crystals: ${earnedCrystals.toLocaleString()} \n<:gold:660257810797428776> Gold Boxes ${caughtGolds.toLocaleString()} \n🕐 Game Time: ${gameTime} \n<:sups:660260925546168404> Supplies: ${suppliesUsage.toLocaleString()}`, true)
+                .addField(`Stats`, `<:Clan_destroyed:660950530096496661> Kills: ${kills.toLocaleString()} \n<:Clan_death:660950530071461890> Deaths: ${deaths.toLocaleString()} \n<:Clan_kd:660950530293497866> K/D: ${kd} \n⚙️ GearScore: ${gearScore}`, true)
 
             message.channel.send({ embed: ratings });
 
