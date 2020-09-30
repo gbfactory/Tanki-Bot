680b84f26a613734b3b8a1363a6f0047b1442909
GB Factory <37420179+gbfactory@users.noreply.github.com>
2020-10-01 00:25:12 +0200

    ✨ Added helper functions

---
+++ utils/functions.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+
+const embedSuccess = (msg, img) => {
+  let embed = new Discord.RichEmbed();
+  embed.setColor('#87d704');
+  if (msg)
+    embed.setDescription('✅ ' + msg);
+  if (img)
+    embed.setThumbnail(img);
+  return embed;
+}
+
+const embedFail = (msg, img) => {
+  let embed = new Discord.RichEmbed();
+  embed.setColor('#f54242');
+  if (msg)
+    embed.setDescription('❌ ' + msg);
+  if (img)
+    embed.setThumbnail(img);
+  return embed;
+}
+
+const embedInfo = (msg, img) => {
+  let embed = new Discord.RichEmbed();
+  embed.setColor('#00c3ff');
+  if (msg)
+    embed.setDescription('❌ ' + msg);
+  if (img)
+    embed.setThumbnail(img);
+  return embed;
+}
+
+const embedRegister = () => {
+  let embed = new Discord.RichEmbed();
+  embed.setColor('#f54242');
+  embed.setDescription("❗️ You are not registered! Use `>register` followed \nby your desired **username** to create a profile.")
+  return embed;
+}
+
+module.exports = {
+  embedSuccess: embedSuccess,
+  embedFail: embedFail,
+  embedInfo: embedInfo,
+  embedRegister: embedRegister
+}
